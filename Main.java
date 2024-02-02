0f2071ddfbbdc4222cf34bd10d0e0a769cb13e54
Enot17451 <52169985+Enot17451@users.noreply.github.com>
2024-02-02 20:10:41 +0500

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,24 +1,30 @@
 import java.io.*;
 import java.net.URL;
 import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
 
 
 public class Main {
     public static void main(String[] args) {
         String url = "https://newlms.magtu.ru/mod/folder/view.php?id=219208";
-        Downloader downloader = new Downloader(url);
+        //Downloader downloader = new Downloader(url);
+
+        char[] c = "<html><a></a><a href=\"adgfad.com\"></a><head><html>".toCharArray();
+        byte[] b = new byte[c.length];
+        for (int i = 0; i < c.length; i++) {
+            b[i] = (byte) c[i];
+        }
+        Help h = new Help();
+        var a = h.getLinksFromArray(b);
     }
 }
 
-class Downloader{
+class Downloader {
 
     String downloadPath;
-    List<String> fileNames = new ArrayList<>();
+    ArrayList<String> fileNames = new ArrayList<>();
 
 
-    public Downloader(String downloadPath){
+    public Downloader(String downloadPath) {
         this.downloadPath = downloadPath;
         parseSite();
     }
@@ -32,7 +38,7 @@ class Downloader{
             System.out.println(str);
 
             Help help = new Help();
-            String a = help.findInArray(bytes,"<a href=");
+            ArrayList<String> a = help.getLinksFromArray(bytes);
 
         } catch (IOException e) {
             System.out.println("Error");
@@ -40,16 +46,41 @@ class Downloader{
     }
 }
 
-class Help{
+class Help {
 
-    String findInArray(byte[] array,String word){
-        StringBuilder builder = new StringBuilder();
-        char[] wordArray = word.toCharArray();
-        for (int i=0;i<array.length;i++){
-            if(array[i]==wordArray[0]){
-                builder.append(array[i]);
+    ArrayList<String> getLinksFromArray(byte[] array) {
+        ArrayList<String> temp = new ArrayList<>();
+        byte[] begin = "<a href=".getBytes();
+        byte[] end = "</a>".getBytes();
+        int i = 0;
+        while (i < array.length) {
+            int indexA = getWordIndex(array, begin, i);
+            if (indexA != -1) {
+                i += indexA;
+                int indexB = getWordIndex(array, end, indexA);
+                temp.add(getWordFromArray(array, indexA, indexB));
+            }
+            i++;
+        }
+        return temp;
+    }
+
+    String getWordFromArray(byte[] array, int indexA, int indexB) {
+        return "";
+    }
+
+    int getWordIndex(byte[] array, byte[] word, int begin) {
+        for (int i = begin; i < array.length; i++) {
+            if (array[i] == word[0]) {
+                for (int j = 1; j < word.length; j++) {
+                    char a = (char) array[i + j];
+                    char b = (char) word[j];
+                    if (array[i + j] != word[j]) {
+                        return -1;
+                    }
+                }
             }
         }
-        return builder.toString();
+        return -1;
     }
 }
